563b4442e7cae556ffaaf5b2ff79945ed1ea8901
Dan Davison <dandavison7@gmail.com>
2022-09-10 07:38:30 -0400

    Open Folders command

--- src/commands.ts
+++ src/commands.ts
@@ -1,3 +1,4 @@
+const { readFile } = require('fs');
 const { spawn } = require('node:child_process');
 const { dirname, resolve } = require('node:path');
 import * as vscode from 'vscode';
@@ -21,4 +22,27 @@ export async function magitStatus(): Promise<void> {
   result.stdout.on('data', (data: string) => {
     log(`stdout: ${data}`);
   });
+}
+
+export async function openFolders(): Promise<void> {
+  const dirs: Set<string> = new Set(
+    (await readFile(expandPath('~/.probable-paths.txt')))
+      .toString()
+      .split('\n')
+      .filter((s: string) => s.length > 0)
+      .map(expandPath)
+      .map(dirname)
+  );
+  for (const dir of dirs.keys()) {
+    let success = await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(dir), {
+      forceNewWindow: true,
+    });
+    if (!success) {
+      log(`Failed to open folder: ${dir}`);
+    }
+  }
+}
+
+function expandPath(path: string): string {
+  return path.replace(/^~/, process.env.HOME || '~');
 }
--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
   const catalog: [string, () => Promise<void>][] = [
     ['process.emacsclient', commands.emacsclient],
     ['process.magitStatus', commands.magitStatus],
+    ['process.openFolders', commands.openFolders],
   ];
   for (const [command, handler] of catalog) {
     context.subscriptions.push(vscode.commands.registerCommand(command, handler));
